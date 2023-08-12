8e97f9aa2042ddedf67b9c0ff454f50edc5bd8f8
Leticia <124852398+LeticiaBHB@users.noreply.github.com>
2023-08-12 14:38:04 -0300

    Update calculo.dart

--- lib/calculo.dart
+++ lib/calculo.dart
@@ -10,28 +10,28 @@ class _CalculoScreenState extends State<CalculoScreen> {
   TextEditingController _controllerGasolina = TextEditingController();
   String _textoResultado = '';
 
-  void _calcular(){
-   double? precoAlcool = double.tryParse(_controllerAlcool.text);
-   double? precoGasolina = double.tryParse(_controllerGasolina.text);
-  if(precoAlcool == null || precoGasolina == null){
-    setState((){
-      _textoResultado = 'Número Inválido';
-    });
+  void _calcular() {
+    double? precoAlcool = double.tryParse(_controllerAlcool.text);
+    double? precoGasolina = double.tryParse(_controllerGasolina.text);
+    if (precoAlcool == null || precoGasolina == null) {
+      setState(() {
+        _textoResultado = 'Número Inválido';
+      });
+    } else {
+      if ((precoAlcool / precoGasolina) >= 0.7) {
+        setState(() {
+          _textoResultado = 'melhor abastecer com gasolina';
+        });
+      } else {
+        setState(() {
+          _textoResultado = 'melhor abastecer com Álcool';
+        });
+      }
+      _limparCampos();
+    }
+  }
 
-  }else{
-   if ((precoAlcool / precoGasolina) >= 0.7){
-     setState((){
-       _textoResultado = 'melhor abastecer com gasolina';
-     });
-   } else {
-     setState((){
-       _textoResultado = 'melhor abastecer com Álcool';
-     });
-   }
-   _limparCampos();
-  }
-  }
-  void _limparCampos(){
+  void _limparCampos() {
     _controllerGasolina.text = '';
     _controllerAlcool.text = '';
   }
@@ -40,20 +40,21 @@ class _CalculoScreenState extends State<CalculoScreen> {
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Container(
             margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
-            width: 250,
+            width: MediaQuery.of(context).size.width * 0.8,
             padding: EdgeInsets.all(16.0),
-                  child: Text(
+            child: Text(
               "Cálculo Vale-a-Pena",
               style: TextStyle(fontSize: 24, color: Colors.black),
               textAlign: TextAlign.center,
             ),
           ),
           Padding(
-            padding: EdgeInsets.only(bottom: 30),
-            child: Image.asset('assets/posto.jpeg', width: 500, height: 250),
+            padding: EdgeInsets.only(bottom: 20),
+            child: Image.asset('assets/posto.jpeg', width: MediaQuery.of(context).size.width * 0.8),
           ),
           Padding(
             padding: EdgeInsets.only(bottom: 10),
@@ -66,23 +67,22 @@ class _CalculoScreenState extends State<CalculoScreen> {
             ),
           ),
           Container(
-            margin: EdgeInsets.fromLTRB(50, 0, 50, 0),
+            margin: EdgeInsets.symmetric(horizontal: 50),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
                 TextField(
                   keyboardType: TextInputType.number,
-                  decoration:
-                      InputDecoration(labelText: 'Preço Alcool, ex: 4.67'),
+                  decoration: InputDecoration(labelText: 'Preço Alcool, ex: 4.67'),
                   style: TextStyle(
                     fontSize: 20,
                   ),
                   controller: _controllerAlcool,
                 ),
+                SizedBox(height: 10),
                 TextField(
                   keyboardType: TextInputType.number,
-                  decoration:
-                      InputDecoration(labelText: 'Preço Gasolina, ex: 5.89'),
+                  decoration: InputDecoration(labelText: 'Preço Gasolina, ex: 5.89'),
                   style: TextStyle(
                     fontSize: 20,
                   ),
@@ -91,25 +91,27 @@ class _CalculoScreenState extends State<CalculoScreen> {
               ],
             ),
           ),
-          Padding(
-            padding: EdgeInsets.only(top: 10),
-            child: ElevatedButton(
-              child: Text('Calcular'),
-              onPressed: () {_calcular();},
-            ),
+          SizedBox(height: 20),
+          ElevatedButton(
+            child: Text('Calcular'),
+            onPressed: () {
+              _calcular();
+            },
           ),
-          Padding(
-              padding: EdgeInsets.only(top:20),
-            child: Text ('$_textoResultado ',
-            style: TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.bold
-            ),
-            ),
+          SizedBox(height: 20),
+          Text(
+            '$_textoResultado ',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
           ),
         ],
       ),
     );
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: CalculoScreen(),
+  ));
 }
 
