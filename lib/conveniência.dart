2aa58cfb5f19b655d25d4ac7c21f2af3d32bafe6
Leticia <124852398+LeticiaBHB@users.noreply.github.com>
2023-08-12 13:49:44 -0300

    Update conveniência.dart

--- lib/conveniência.dart
+++ lib/conveniência.dart
@@ -10,6 +10,12 @@ class ConvenienciaScreen extends StatefulWidget {
 class _ConvenienciaScreenState extends State<ConvenienciaScreen> {
   bool _trocadeoleo = false;
   bool _alinhamentoEBalanceamento = false;
+  bool _trocedefiltro = false;
+  bool _revisaofreios = false;
+  String? _escolhaUsuario;
+  bool _notificacao = false;
+  double valor = 5;
+  String label = '5';
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +27,7 @@ class _ConvenienciaScreenState extends State<ConvenienciaScreen> {
               children: <Widget>[
                 CheckboxListTile(
                   title: Text("Troca de Óleo"),
-                  subtitle: Text('o melhor da região'),
+                  subtitle: Text('na mecânica do posto'),
                   activeColor: Colors.redAccent,
                   secondary: Icon(Icons.cabin_outlined),
                   value: _trocadeoleo,
@@ -48,10 +54,10 @@ class _ConvenienciaScreenState extends State<ConvenienciaScreen> {
                   subtitle: Text('na mecânica do posto'),
                   activeColor: Colors.redAccent,
                   secondary: Icon(Icons.cabin_outlined),
-                  value: _alinhamentoEBalanceamento,
+                  value: _trocedefiltro,
                   onChanged: (bool? valor) {
                     setState(() {
-                      _alinhamentoEBalanceamento = valor!;
+                      _trocedefiltro = valor!;
                     });
                   },
                 ),
@@ -60,28 +66,96 @@ class _ConvenienciaScreenState extends State<ConvenienciaScreen> {
                   subtitle: Text('na mecânica do posto'),
                   activeColor: Colors.redAccent,
                   secondary: Icon(Icons.cabin_outlined),
-                  value: _alinhamentoEBalanceamento,
+                  value: _revisaofreios,
                   onChanged: (bool? valor) {
                     setState(() {
-                      _alinhamentoEBalanceamento = valor!;
+                      _revisaofreios = valor!;
                     });
                   },
                 ),
-                ElevatedButton(
-                  onPressed: () {
-                    // Coloque o código para salvar aqui
-                  },
-                  child: Text(
-                    'Salvar',
-                    style: TextStyle(fontSize: 20),
-                  ),
+                Row(
+                  children: [
+                    SizedBox(width: 60),
+                    Text('Posto EcoDrive Fuels'),
+                    Radio(
+                      value: 'EcoDrive Fuels',
+                      groupValue: _escolhaUsuario,
+                      onChanged: (String? escolha) {
+                        setState(() {
+                          _escolhaUsuario = escolha!;
+                        });
+                        print('resultado' + escolha!);
+                      },
+                    ),
+                    Text('Posto VelocityVista Gas'),
+                    Radio(
+                      value: 'VelocityVista Gas',
+                      groupValue: _escolhaUsuario,
+                      onChanged: (String? escolha) {
+                        setState(() {
+                          _escolhaUsuario = escolha!;
+                        });
+                        print('resultado' + escolha!);
+                      },
+                    ),
+                  ],
                 ),
               ],
             ),
           ),
-          Container(
-
+            Row(
+              children: [
+                SizedBox(width: 60),
+                Switch(
+                    value: _notificacao ,
+                    onChanged: (bool valor){
+                      setState(() {
+                        _notificacao = valor;
+                      });
+                    },
+                ),
+                Text('Receber notificações?'),
+              ],
+            ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text(
+              'Salvar',
+              style: TextStyle(fontSize: 20),
+            ),
           ),
+          SizedBox(height: 100),
+          Column(
+            children: [
+              Container(
+                child: Text('Quanto você está satisfeito com nosso aplicativo?')
+              ),
+              Slider(
+                  value: valor,
+                  min: 0,
+                  max: 10,
+                  label: label,
+                  divisions: 10,
+                  activeColor: Colors.redAccent,
+                  inactiveColor: Colors.black26,
+                  onChanged: (double novoValor){
+                    setState(() {
+                      valor = novoValor;
+                      label = novoValor.toString();
+                    });
+                  }
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  print('valor selecionado:' + valor.toString());
+                },
+                child: Text(
+                  'Enviar',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+            ],
+          )
         ],
       ),
     );
